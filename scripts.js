4ee41a5a4299d07e75c29f7451b5e2826b316bc2
AnnaKauker <annakauker@icloud.com>
2022-02-23 13:08:56 +0100

    Adding new jekyll test

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// A $( document ).ready() block.
+$(document).ready(function() {
+
+        $(".hamburger-button").click(function() {
+            $(this).toggleClass("active");
+            $(".mobile-menu").fadeToggle()
+        });
+
+        AOS.init();
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
